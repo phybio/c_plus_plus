39006123c02a611cc6661ea94a5e21a69e26d6e4
phybio <wisedd@outlook.com>
2014-09-12 16:55:40 +0800

    构造函数在类内

--- test/simple/main.cpp
+++ test/simple/main.cpp
@@ -3,30 +3,18 @@ using namespace std;
 class Base1
 {
 public:
-	Base1();
-	virtual ~Base1();
-	virtual void speakClearly();
-	virtual Base1 *clone() const;
+	Base1() {};
+	virtual ~Base1() {};
 	virtual int function();
+	int define_not_call();
 protected:
 	float data_Base1;
 };
 
-Base1::Base1()
-{
-
-}
-
-#if 0
-Base1 *Base1::clone() const
-{
-	cout << "Base1 clone.";
-}
-#endif
-
 int Base1::function()
 {
-	cout << "Base1 function.";
+	cout << "Base1 function." << endl;
+	return 1;
 }
 
 class Base2
@@ -34,51 +22,30 @@ class Base2
 public:
 	Base2();
 	virtual ~Base2();
-	virtual void mumble();
-	virtual Base2 *clone() const;
 	virtual int function();
 protected:
 	float data_Base2;
 };
 
-Base2::Base2()
-{
-
-}
-
-#if 0
-Base2 *Base2::clone() const
-{
-	cout << "Base2 clone.";
-}
-#endif
-
 int Base2::function()
 {
-	cout << "Base2 function.";
+	cout << "Base2 function." << endl;
 }
 
-class Derived : public Base1, public Base2
+class Derived : public Base1
 {
 public:
-	Derived();
-	virtual ~Derived();
-	virtual Derived *clone() const;
+	Derived() {};
+	virtual ~Derived() {};
+	int define_not_call();
 protected:
 	float data_Derived;
 };
 
-#if 0
-Base1 *Derived::clone() const
-{
-	cout << "Derived clone."; 
-}
-#endif
-
 int main(int argc, char *argv[])
 {
 	cout << "Hello world!" << endl;
-	Base1 theDerived;
+	Derived theDerived;
 	theDerived.function();
 	return 0;
 }
