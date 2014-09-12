1acd93892cc0597853b300cef3190ccecf6936da
phybio <wisedd@outlook.com>
2014-09-12 17:01:11 +0800

    还有编译问题，先提交上去

--- book/inside_the_c_plus_plus_object_model/chapter_04/virtual_functions/main.cpp
+++ book/inside_the_c_plus_plus_object_model/chapter_04/virtual_functions/main.cpp
@@ -7,9 +7,27 @@ public:
 	virtual ~Base1();
 	virtual void speakClearly();
 	virtual Base1 *clone() const;
+	virtual int function();
 protected:
 	float data_Base1;
 };
+
+Base1::Base1()
+{
+
+}
+
+#if 0
+Base1 *Base1::clone() const
+{
+	cout << "Base1 clone.";
+}
+#endif
+
+int Base1::function()
+{
+	cout << "Base1 function.";
+}
 
 class Base2
 {
@@ -18,9 +36,27 @@ public:
 	virtual ~Base2();
 	virtual void mumble();
 	virtual Base2 *clone() const;
+	virtual int function();
 protected:
 	float data_Base2;
 };
+
+Base2::Base2()
+{
+
+}
+
+#if 0
+Base2 *Base2::clone() const
+{
+	cout << "Base2 clone.";
+}
+#endif
+
+int Base2::function()
+{
+	cout << "Base2 function.";
+}
 
 class Derived : public Base1, public Base2
 {
@@ -32,8 +68,17 @@ protected:
 	float data_Derived;
 };
 
+#if 0
+Base1 *Derived::clone() const
+{
+	cout << "Derived clone."; 
+}
+#endif
+
 int main(int argc, char *argv[])
 {
 	cout << "Hello world!" << endl;
+	Derived *theDerived = new Derived;
+	delete theDerived; 
 	return 0;
 }
