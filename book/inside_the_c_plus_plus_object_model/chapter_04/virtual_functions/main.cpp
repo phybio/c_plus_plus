9fafee39eae3996c081a59c4bfa6e5089382e8d4
phybio <wisedd@outlook.com>
2014-09-12 15:07:48 +0800

    add virtual function demo

---
+++ book/inside_the_c_plus_plus_object_model/chapter_04/virtual_functions/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Base1
+{
+public:
+	Base1();
+	virtual ~Base1();
+	virtual void speakClearly();
+	virtual Base1 *clone() const;
+protected:
+	float data_Base1;
+};
+
+class Base2
+{
+public:
+	Base2();
+	virtual ~Base2();
+	virtual void mumble();
+	virtual Base2 *clone() const;
+protected:
+	float data_Base2;
+};
+
+class Derived : public Base1, public Base2
+{
+public:
+	Derived();
+	virtual ~Derived();
+	virtual Derived *clone() const;
+protected:
+	float data_Derived;
+};
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
