e5efd6a05e296ac13b6694959bc126a008daaabd
phybio <wisedd@outlook.com>
2014-10-02 08:48:35 +0800

    add virtual multiply inherent

---
+++ book/think_and_test/multi_inherent_03/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class Base1
+{
+public:
+	Base1() { cout << "Base1()" << endl; };
+	virtual ~Base1() { cout << "~Base1()" << endl; };
+	void speakClearly() { cout << "speakClearly()" << endl; };
+protected:
+	float data_Base1;
+};
+
+class Base2 : virtual public Base1
+{
+public:
+       	Base2() { cout << "Base2()" << endl; };
+	virtual ~Base2() { cout << "~Base2()" << endl; };
+};
+
+class Base3 : virtual public Base1
+{
+public:
+       	Base3() { cout << "Base3()" << endl; };
+	virtual ~Base3() { cout << "~Base()" << endl; };
+};
+
+class Derived : virtual public Base1, virtual public Base2
+{
+public:
+	Derived() { cout << "Derived()" << endl; };
+	virtual ~Derived() { cout << "~Derived()" << endl; };
+protected:
+	float data_Derived;
+};
+
+int main(int argc, char *argv[])
+{
+	Derived *ptr = new Derived;
+	delete ptr;
+	ptr = NULL;
+	cout << "Hello world!" << endl;
+	return 0;
+}
